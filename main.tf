5593087753c28442a701ce54d6a6b924a03fb600
gasgithub <gasgithub@users.noreply.github.com>
2022-03-30 18:12:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "curl-host" {
   provisioner "local-exec" {
     command = <<-EOT
       echo "Curling ${var.hostname}"
-      curl -v -k https://${var.name}
+      curl -v -k https://${var.hostname}
     EOT
   }
 }
