1ab4f02bf5a201c65b0d79493c7b817c23ee6ca3
gasgithub <gasgithub@users.noreply.github.com>
2022-04-08 16:02:50 +0200

    added output ip

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "datacenter_id" {
 output "vm_id" {
     value = vsphere_virtual_machine.vm.id
 }
+output "vm_ip" {
+    value = vsphere_virtual_machine.vm.*.default_ip_address
+    #vsphere_virtual_machine.vm.*.clone.0.customize.0.network_interface.0.ipv4_address
+}
